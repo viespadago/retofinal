caf3ec7854c5356747590e5c83fea212f39b6214
FrankPalomino <frank.palomino1@gmail.com>
2021-12-10 10:32:14 +0100

    Primer commit backend

---
+++ frontend/src/main/java/com/main/nuewcash/NuewcashApplication.java
@@ -0,0 +1,13 @@
+package com.main.nuewcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuewcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuewcashApplication.class, args);
+	}
+
+}
---
+++ frontend/src/test/java/com/main/nuewcash/NuewcashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.nuewcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuewcashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
