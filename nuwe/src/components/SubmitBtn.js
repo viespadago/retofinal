2a835a6018d0b0cfff4368065bf08f6b4c62c3df
Sararid <sarawen88@gmail.com>
2021-12-10 12:46:54 +0100

    add button component and dashboard

---
+++ nuwe/src/components/SubmitBtn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const SubmitBtn = () => {
+
+    //fecth al back y nos devuelve 
+    return (
+        <>
+            <button>UPDATE DATA</button>
+        </>
+    )
+}
+export default SubmitBtn;
